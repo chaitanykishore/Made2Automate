0315bc7ca3823eb8a4e7265048c80f67f01636bb
chaitanya kishore <ck3642013@gmail.com>
2023-12-21 02:16:53 +0530

    first commit

---
+++ sql/Product.sql
@@ -0,0 +1,8 @@
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_image VARCHAR(255) NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    product_id VARCHAR(50) NOT NULL,
+    manufacture_name VARCHAR(100) NOT NULL,
+    quantity INT NOT NULL
+);
